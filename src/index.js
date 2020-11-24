3624ed05abc493fc307dba23755de0f319db39e4
mollymoog <mmkover@gmail.com>
2020-11-24 17:14:38 -0500

    commit tree 869ec1bb23517cef5e18a3e00a6b82507abd57fa

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function fetchDog() {
+    return fetch(imgUrl)
+        .then(resp => resp.json())
+        .then(json => renderImg(json));
+}
+
+function renderImg(images) {
+    
+}
